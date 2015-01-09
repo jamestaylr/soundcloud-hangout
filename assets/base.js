269218838f4ac56bc74d39ab9c0f37cdd1e34d3f
James Taylor <jamestaylr@gmail.com>
2015-01-09 18:41:08 -0500

    Added event handlers
    There are event handlers for when the API has loaded, the participants
    have changed, and when the state of the application has changed.

--- assets/base.js
+++ assets/base.js
@@ -1,17 +1,28 @@
 function init() {
-    // When API is ready... 
-    gapi.hangout.onApiReady.add(
-        function(eventObj) {
-            if (eventObj.isApiReady) {
-                document.getElementById('playing')
-                    .style.visibility = 'visible';
-            }
-        });
+	// When API is ready...
+	gapi.hangout.onApiReady.add(applicationReady);
+}
+
+applicationReady = function(event) {
+	if (event.isApiReady) {
+		document.getElementById('playing').style.visibility = 'visible';
+		gapi.hangout.onParticipantsChanged.add(participantsChanged);
+		gapi.hangout.data.onStateChanged.add(stateChanged);
+	}
+}
+
+participantsChanged = function(event) {
+	
+}
+
+stateChanged = function(event) {
+	
 }
 
 function createEmbededSound() {
 	var pane = document.getElementById('playing');
+	
 }
 
-// Wait for gadget to load.                                                       
+// Wait for gadget to load.
 gadgets.util.registerOnLoadHandler(init);

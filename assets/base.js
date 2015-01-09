5827fa387ae48b813e5072413dca963f5703899f
James Taylor <jamestaylr@gmail.com>
2015-01-09 15:02:51 -0500

    Moved scripts to external files
    Created main Javascript class `base.js` to run the application.

---
+++ assets/base.js
@@ -0,0 +1,13 @@
+function init() {
+    // When API is ready... 
+    gapi.hangout.onApiReady.add(
+        function(eventObj) {
+            if (eventObj.isApiReady) {
+                document.getElementById('playing')
+                    .style.visibility = 'visible';
+            }
+        });
+}
+
+// Wait for gadget to load.                                                       
+gadgets.util.registerOnLoadHandler(init);

014ed57bfc68c0e85ade73562af5f9f2ac8022a3
James Taylor <jamestaylr@gmail.com>
2015-01-09 22:17:45 -0500

    Major aesthetic and formatting changes

--- assets/base.js
+++ assets/base.js
@@ -12,35 +12,33 @@ applicationReady = function(event) {
 }
 
 participantsChanged = function(event) {
-	
+
 }
 
 stateChanged = function(event) {
-	
+
 }
 
 function createEmbededSound() {
 	var pane = document.getElementById('playing');
-	var so = new SoundObject('https://soundcloud.com/fuckmylive/the-reward-is-cheese-x-daft');
-	pane.innerHTML = so.getInfo();
-	
+
 }
 
-function encodeURI(uri)
-{
-	return encodeURIComponent(uri).replace(/'/g,"%27").replace(/"/g,"%22");
+function encodeURI(uri) {
+	return encodeURIComponent(uri).replace(/'/g, "%27").replace(/"/g, "%22");
 }
 
 // Wait for gadget to load.
 gadgets.util.registerOnLoadHandler(init);
 
-// ----------------------------------------
+//-------------------------------------------------------------------------
 
-function SoundObject (uri) {
-    this.uri = uri;
+function SoundObject(uri) {
+	this.uri = uri;
 
-    this.getInfo = function() {
-        return '<iframe scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=' +
-        encodeURI(this.uri) + '&amp;auto_play=false&amp;show_artwork=true&amp;color=0066cc"></iframe>';
-    };
+	this.getInfo = function() {
+		return '<iframe scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url='
+				+ encodeURI(this.uri)
+				+ '&amp;auto_play=false&amp;show_artwork=true&amp;color=0066cc"></iframe>';
+	};
 }

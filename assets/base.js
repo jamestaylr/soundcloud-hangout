14223c1ba04862fd1aab729875321d48835b0220
James Taylor <jamestaylr@gmail.com>
2015-01-11 01:30:18 -0500

    Created event framework; fixed API loading instability
    The widget is now properly bound to state events. Typing a URL will
    load a track. Load events, pause events, and play events should effect
    all users who have the app open.

--- assets/base.js
+++ assets/base.js
@@ -8,11 +8,23 @@ gadgets.util.registerOnLoadHandler(init);
 function init() {
 	// When API is ready...
 	gapi.hangout.onApiReady.add(applicationReady);
+
+	SC.initialize({
+		client_id : client_id,
+	});
+
+	setupWidget();
+
 }
 
-SC.initialize({
-	client_id : client_id,
-});
+function setupWidget() {
+	var frame = document.getElementById('player');
+	widget = SC.Widget(frame);
+
+	widget.bind(SC.Widget.Events.PLAY, playerPlay);
+	widget.bind(SC.Widget.Events.FINISH, playerFinish);
+	widget.bind(SC.Widget.Events.PAUSE, playerPause);
+}
 
 applicationReady = function(event) {
 	if (event.isApiReady) {
@@ -26,26 +38,51 @@ participantsChanged = function(event) {
 }
 
 // -------------------------------------------------------------------------
+var playingTrack;
+var isPlaying;
 
 stateChanged = function(event) {
 
-	var value = gapi.hangout.data.getValue("current");
-	var track = JSON.parse(value);
-	tracks.push(track);
+	var parsedTrack = JSON.parse(gapi.hangout.data.getValue('add-track'));
+	var parsedState = JSON.parse(gapi.hangout.data.getValue('state'));
 
-	
+	if (!(isPlaying == parsedState)) {
+		if (isPlaying) {
+			pauseTrack();
+		} else {
+			playTrack();
+		}
+
+		isPlaying = !isPlaying;
+	}
+
+	loadTrack(track);
+
 }
 
-playerReady = function() {
+function loadTrack(track) {
+	var query = track.uri + "?client_id=" + client_id + "&amp;auto_play=true";
+	widget.load(query);
+	isPlaying = true;
 
-	// add event listerners
-	widget.bind(SC.Widget.Events.PLAY, playerPlay);
-	widget.bind(SC.Widget.Events.FINISH, playerFinish);
+}
 
+function playTrack() {
+	widget.play();
+}
+
+function pauseTrack() {
+	widget.pause();
 }
 
 playerPlay = function() {
+	isPlaying = true;
+	gapi.hangout.data.setValue('state', JSON.stringify(state));
+}
 
+playerPause = function() {
+	isPlaying = false;
+	gapi.hangout.data.setValue('state', JSON.stringify(state));
 }
 
 playerFinish = function() {
@@ -59,10 +96,9 @@ function createEmbededSound(event) {
 	var permalink_url = encodeURI(event.target.value);
 
 	SC.get('https://api.soundcloud.com/resolve.json?url=' + permalink_url
-			+ '/tracks&client_id=' + client_id,
-			function(result) {
-				gapi.hangout.data.setValue("current", JSON.stringify(result));
-			});
+			+ '/tracks&client_id=' + client_id, function(result) {
+		gapi.hangout.data.setValue('add-track', JSON.stringify(result));
+	});
 
 }
 

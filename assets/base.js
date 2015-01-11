04d8d99a9ed92d528bd34f96c29c7bde4e18a422
James Taylor <jamestaylr@gmail.com>
2015-01-10 19:19:11 -0500

    Implemented Soundcloud Javascript SDK
    The URL entered into the text field is resolved into a Soundcloud
    `Object` by calling `resolve.json` in the Soundcloud API. This is then
    converted into JSON and causes the `stateChanged` Hangouts function to
    be triggered.

--- assets/base.js
+++ assets/base.js
@@ -1,7 +1,18 @@
+var widget;
+var client_id = "";
+var tracks = [];
+
+// Wait for gadget to load.
+gadgets.util.registerOnLoadHandler(init);
+
 function init() {
 	// When API is ready...
 	gapi.hangout.onApiReady.add(applicationReady);
 }
+
+SC.initialize({
+	client_id : client_id,
+});
 
 applicationReady = function(event) {
 	if (event.isApiReady) {
@@ -14,40 +25,47 @@ participantsChanged = function(event) {
 
 }
 
+// -------------------------------------------------------------------------
+
 stateChanged = function(event) {
 
 	var value = gapi.hangout.data.getValue("current");
+	var track = JSON.parse(value);
+	tracks.push(track);
 
-	alert('queued!');
-	var a = new SoundObject(value);
-	document.getElementById('player').innerHTML = a.getInfo();
+	
 }
 
-function createEmbededSound(event) {
-	
-	var uri = encodeURI(event.target.value);
-	
-	var so = new SoundObject(uri);
+playerReady = function() {
 
-	gapi.hangout.data.setValue("current", uri);
+	// add event listerners
+	widget.bind(SC.Widget.Events.PLAY, playerPlay);
+	widget.bind(SC.Widget.Events.FINISH, playerFinish);
+
+}
+
+playerPlay = function() {
+
+}
+
+playerFinish = function() {
+
+}
+
+// -------------------------------------------------------------------------
+
+function createEmbededSound(event) {
+
+	var permalink_url = encodeURI(event.target.value);
+
+	SC.get('https://api.soundcloud.com/resolve.json?url=' + permalink_url
+			+ '/tracks&client_id=' + client_id,
+			function(result) {
+				gapi.hangout.data.setValue("current", JSON.stringify(result));
+			});
 
 }
 
 function encodeURI(uri) {
 	return encodeURIComponent(uri).replace(/'/g, "%27").replace(/"/g, "%22");
-}
-
-// Wait for gadget to load.
-gadgets.util.registerOnLoadHandler(init);
-
-// -------------------------------------------------------------------------
-
-function SoundObject(uri) {
-	this.uri = uri;
-
-	this.getInfo = function() {
-		return '<iframe scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url='
-				+ encodeURI(this.uri)
-				+ '&amp;auto_play=false&amp;show_artwork=true&amp;color=0066cc"></iframe>';
-	};
 }

70e190c2788efb57aa3f668c5484b5845993af7f
James Taylor <jamestaylr@gmail.com>
2015-01-22 14:02:21 -0500

    Formatting and minor stylistic changes

--- assets/base.js
+++ assets/base.js
@@ -5,51 +5,51 @@ var client_id = "";
 gadgets.util.registerOnLoadHandler(init);
 
 function init() {
-	// When API is ready...
-	gapi.hangout.onApiReady.add(applicationReady);
+    // When API is ready...
+    gapi.hangout.onApiReady.add(applicationReady);
 
-	SC.initialize({
-		client_id : client_id,
-	});
+    SC.initialize({
+        client_id: client_id,
+    });
 
 }
 
 function setupWidget() {
-	var frame = document.getElementsByTagName("iframe")[0];
-	widget = SC.Widget(frame);
+    var frame = document.getElementsByTagName("iframe")[0];
+    widget = SC.Widget(frame);
 
-	widget.bind(SC.Widget.Events.PLAY, playerPlay);
-	widget.bind(SC.Widget.Events.FINISH, playerFinish);
-	widget.bind(SC.Widget.Events.PAUSE, playerPause);
-	widget.bind(SC.Widget.Events.SEEK, playerSeek);
+    widget.bind(SC.Widget.Events.PLAY, playerPlay);
+    widget.bind(SC.Widget.Events.FINISH, playerFinish);
+    widget.bind(SC.Widget.Events.PAUSE, playerPause);
+    widget.bind(SC.Widget.Events.SEEK, playerSeek);
 
 }
 
 applicationReady = function(event) {
-	if (event.isApiReady) {
-		gapi.hangout.onParticipantsEnabled.add(participantsEnabled);
-		gapi.hangout.data.onStateChanged.add(stateChanged);
+    if (event.isApiReady) {
+        gapi.hangout.onParticipantsEnabled.add(participantsEnabled);
+        gapi.hangout.data.onStateChanged.add(stateChanged);
 
-		var data = gapi.hangout.data.getStateMetadata();
+        var data = gapi.hangout.data.getStateMetadata();
 
-		if ('applicationState' in data) {
-			var state = JSON.parse(data.applicationState.value);
-			embedWidget(state[1]);
+        if ('applicationState' in data) {
+            var state = JSON.parse(data.applicationState.value);
+            embedWidget(state[1]);
 
-			for (var i = 0; i < state[0].length; i++) {
-				queueTrack(state[0][i]);
-			}
+            for (var i = 0; i < state[0].length; i++) {
+                queueTrack(state[0][i]);
+            }
 
-		}
-	}
+        }
+    }
 }
 
 participantsEnabled = function(event) {
-	var state = [];
-	state.push(tracks);
-	state.push(current);
+    var state = [];
+    state.push(tracks);
+    state.push(current);
 
-	gapi.hangout.data.setValue('applicationState', JSON.stringify(state));
+    gapi.hangout.data.setValue('applicationState', JSON.stringify(state));
 
 }
 
@@ -59,188 +59,183 @@ var tracks = [];
 
 stateChanged = function(event) {
 
-	var changed = getRecentObject(gapi.hangout.data.getStateMetadata());
-	var key = changed.key;
+    var changed = getRecentObject(gapi.hangout.data.getStateMetadata());
+    var key = changed.key;
 
-	if (key == 'state') {
-		var state = JSON.parse(changed.value);
+    if (key == 'state') {
+        var state = JSON.parse(changed.value);
 
-		widget.isPaused(function(data) {
-			var playing = !data;
-			if (!(playing == state)) {
-				if (playing) {
-					widget.pause();
-				} else {
-					widget.play();
-				}
-			}
-		});
+        widget.isPaused(function(data) {
+            var playing = !data;
+            if (!(playing == state)) {
+                if (playing) {
+                    widget.pause();
+                } else {
+                    widget.play();
+                }
+            }
+        });
 
-	} else if (key == 'add-track') {
-		var parsedTrack = JSON.parse(changed.value);
+    } else if (key == 'add-track') {
+        var parsedTrack = JSON.parse(changed.value);
 
-		if (widget == undefined) {
-			embedWidget(parsedTrack);
-			current = parsedTrack;
+        if (widget == undefined) {
+            embedWidget(parsedTrack);
+            current = parsedTrack;
 
-		} else {
+        } else {
 
-			if (current == undefined) {
-				loadTrack(parsedTrack);
-			} else {
-				tracks.push(parsedTrack);
-				queueTrack(parsedTrack);
-			}
+            if (current == undefined) {
+                loadTrack(parsedTrack);
+            } else {
+                tracks.push(parsedTrack);
+                queueTrack(parsedTrack);
+            }
 
-		}
+        }
 
-	} else if (key == 'remove-track') {
-		var id = JSON.parse(changed.value);
+    } else if (key == 'remove-track') {
+        var id = JSON.parse(changed.value);
 
-		var i = 0;
+        var i = 0;
 
-		for (; i < tracks.length; i++) {
-			if (tracks[i].id == id) {
-				break;
-			}
-		}
+        for (; i < tracks.length; i++) {
+            if (tracks[i].id == id) {
+                break;
+            }
+        }
 
-		tracks.splice(i, 1);
-		document.getElementById('queue').deleteRow(i);
+        tracks.splice(i, 1);
+        document.getElementById('queue').deleteRow(i);
 
-	} else if (key == 'seek') {
-		var state = JSON.parse(changed.value);
+    } else if (key == 'seek') {
+        var state = JSON.parse(changed.value);
 
-		if (state.relativePosition > 0.995) {
+        if (state.relativePosition > 0.995) {
 
-			current = undefined;
+            current = undefined;
 
-			if (tracks.length > 0) {
-				loadTrack(tracks.shift());
-				document.getElementById('queue').deleteRow(0);
-			}
-			return;
-		}
+            if (tracks.length > 0) {
+                loadTrack(tracks.shift());
+                document.getElementById('queue').deleteRow(0);
+            }
+            return;
+        }
 
-		widget
-				.getPosition(function(data) {
-					if (Math.abs(parseInt(data)
-							- parseInt(state.currentPosition)) > 1000) {
-						widget.seekTo(state.currentPosition);
-					}
-				});
-	}
+        widget
+            .getPosition(function(data) {
+                if (Math.abs(parseInt(data) - parseInt(state.currentPosition)) >
+                    1000) {
+                    widget.seekTo(state.currentPosition);
+                }
+            });
+    }
 }
 
 function getRecentObject(data) {
 
-	var recent;
+    var recent;
 
-	for ( var key in data) {
-		var object = data[key];
+    for (var key in data) {
+        var object = data[key];
 
-		try {
-			if (parseInt(object.timestamp) > parseInt(recent.timestamp)) {
-				recent = object;
-			}
-		} catch (TypeError) {
-			recent = object;
-		}
-	}
+        try {
+            if (parseInt(object.timestamp) > parseInt(recent.timestamp)) {
+                recent = object;
+            }
+        } catch (TypeError) {
+            recent = object;
+        }
+    }
 
-	return recent;
+    return recent;
 }
 
 function embedWidget(initialTrack) {
-	SC.oEmbed(initialTrack.uri, {
-		auto_play : true
-	},
-			function(oembed) {
-				document.getElementById('target').innerHTML = oembed.html
-						.replace('visual=true&', '').replace('height=\"400\"',
-								'height=\"144\"');
+    SC.oEmbed(initialTrack.uri, {
+            auto_play: true
+        },
+        function(oembed) {
+            document.getElementById('target').innerHTML = oembed.html
+                .replace('visual=true&', '').replace('height=\"400\"',
+                    'height=\"144\"');
 
-				setupWidget();
-			});
+            setupWidget();
+        });
 }
 
 function loadTrack(track) {
-	current = track;
-	var query = track.uri + "?client_id=" + client_id + "&amp;auto_play=true";
-	widget.load(query);
+    current = track;
+    var query = track.uri + "?client_id=" + client_id +
+        "&amp;auto_play=true";
+    widget.load(query);
 }
 
 function queueTrack(track) {
-	var j = document.getElementById('queue');
+    var j = document.getElementById('queue');
 
-	var seconds = Math.trunc(track.duration / 1000);
-	var minutes = Math.trunc(seconds / 60);
-	var artwork = track.artwork_url;
+    var seconds = Math.trunc(track.duration / 1000);
+    var minutes = Math.trunc(seconds / 60);
+    var artwork = track.artwork_url;
 
-	j.innerHTML += '<tr id=\"'
-			+ track.id
-			+ '\"><td>'
-			+ '<img src=\"'
-			+ artwork
-			+ '\" \\>'
-			+ '</td><td class=\"title\">'
-			+ track.title
-			+ ' by '
-			+ '<a href=\"'
-			+ track.user.uri
-			+ '\">'
-			+ track.user.username
-			+ '</a>'
-			+ '</td><td class=\"time\">'
-			+ minutes
-			+ ':'
-			+ (((seconds % 60) / 100).toFixed(2).toString()).replace('0.', '')
-			+ '</td><td class=\"likes\">'
-			+ track.favoritings_count.toString().replace(
-					/\B(?=(\d{3})+(?!\d))/g, ",")
-			+ '</td><td class=\"plays\">'
-			+ track.playback_count.toString().replace(/\B(?=(\d{3})+(?!\d))/g,
-					",") + '</td><td class=\"close\">' + '</td></tr>';
+     var result = '<tr id=\"' + track.id + '\"><td>';
+    
+    
+    if (artwork != undefined)
+    {
+    	result += '<img src=\"' + artwork + '\" \\>';
+    }
+    
+    result += '</td><td class=\"title\">' + track.title +
+        ' by ' + '<a href=\"' + track.user.uri + '\">' + track.user.username +
+        '</a>' + '</td><td class=\"time\">' + minutes + ':' + (((seconds %
+            60) / 100).toFixed(2).toString()).replace('0.', '') +
+        '</td><td class=\"likes\">' + track.favoritings_count.toString().replace(
+            /\B(?=(\d{3})+(?!\d))/g, ",") + '</td><td class=\"plays\">' +
+        track.playback_count.toString().replace(/\B(?=(\d{3})+(?!\d))/g,
+            ",") + '</td><td class=\"close\">' + '</td></tr>';
+        
+        j.innerHTML += result;
 }
 
 var searchResults = [];
 
 document.body.onclick = function(e) {
-	e = window.event ? event.srcElement : e.target;
-	if (e.className && e.className.indexOf('close') != -1) {
-		var id = e.parentNode.id;
-		gapi.hangout.data.setValue('remove-track', JSON.stringify(id));
-	} else if (e.className && e.className.indexOf('result') != -1) {
-		var value = e.value.toString();
+    e = window.event ? event.srcElement : e.target;
+    if (e.className && e.className.indexOf('close') != -1) {
+        var id = e.parentNode.id;
+        gapi.hangout.data.setValue('remove-track', JSON.stringify(id));
+    } else if (e.className && e.className.indexOf('result') != -1) {
+        var value = e.value.toString();
 
-		for (var i = 0; i < searchResults.length; i++) {
-			if (value == searchResults[i].id) {
-				gapi.hangout.data.setValue('add-track', JSON
-						.stringify(searchResults[i]));
-				break;
-			}
-		}
+        for (var i = 0; i < searchResults.length; i++) {
+            if (value == searchResults[i].id) {
+                gapi.hangout.data.setValue('add-track', JSON
+                    .stringify(searchResults[i]));
+                break;
+            }
+        }
 
-		document.getElementById('search-results').innerHTML = "";
-	}
+        document.getElementById('search-results').innerHTML = "";
+    }
 
-	document.getElementById('search-results').innerHTML = "";
+    document.getElementById('search-results').innerHTML = "";
 }
 
 playerPlay = function() {
-	gapi.hangout.data.setValue('state', JSON.stringify(true));
+    gapi.hangout.data.setValue('state', JSON.stringify(true));
 }
 
 playerPause = function() {
-	gapi.hangout.data.setValue('state', JSON.stringify(false));
+    gapi.hangout.data.setValue('state', JSON.stringify(false));
 }
 
 playerSeek = function(data) {
-	gapi.hangout.data.setValue('seek', JSON.stringify(data));
+    gapi.hangout.data.setValue('seek', JSON.stringify(data));
 }
 
 playerFinish = function(data) {
-	gapi.hangout.data.setValue('seek', JSON.stringify(data));
+    gapi.hangout.data.setValue('seek', JSON.stringify(data));
 
 }
 
@@ -248,23 +243,24 @@ playerFinish = function(data) {
 
 function createEmbededSound(event) {
 
-	SC.get('/tracks', {
-		q : event.target.value
-	}, function(tracks) {
-		searchResults = [];
+    SC.get('/tracks', {
+        q: event.target.value
+    }, function(tracks) {
+        searchResults = [];
 
-		var r = document.getElementById('search-results');
-		r.innerHTML = "";
+        var r = document.getElementById('search-results');
+        r.innerHTML = "";
 
-		for (var i = 0; (i < tracks.length) && (i < 5); i++) {
-			searchResults.push(tracks[i]);
-			r.innerHTML += '<li class=\"result\" value=\"' + tracks[i].id
-					+ '\">' + tracks[i].title + '</li>';
-		}
-	});
+        for (var i = 0;
+            (i < tracks.length) && (i < 5); i++) {
+            searchResults.push(tracks[i]);
+            r.innerHTML += '<li class=\"result\" value=\"' + tracks[
+                i].id + '\">' + tracks[i].title + '</li>';
+        }
+    });
 
 }
 
 function encodeURI(uri) {
-	return encodeURIComponent(uri).replace(/'/g, "%27").replace(/"/g, "%22");
+    return encodeURIComponent(uri).replace(/'/g, "%27").replace(/"/g, "%22");
 }
